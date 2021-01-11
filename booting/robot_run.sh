561b26af2e884514817d35b80cd511caae0ecca0
ladianchad <qhrejddlvltm@gmail.com>
2021-01-11 08:13:44 +0000

    msg boot update

--- booting/robot_run.sh
+++ booting/robot_run.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 source /opt/ros/noetic/setup.bash 
 source /home/ubuntu/life_ws/devel/setup.bash
+gpio -g mode 17 output
+gpio -g mode 22 output
+gpio -g mode 27 output
+gpio -g write 17 0
+gpio -g write 27 0
+gpio -g write 22 0
+sleep 1
 
-roscore &
-sudo chmod 777 /dev/ttyUSB*
+gpio -g write 17 1
+sleep 0.2
+roscore 
 
-sleep 2
 
+#gpio -g write 17 0
+#sleep 0.2
+#gpio -g write 27 1
 
-rosrun gps_n_lora gps_n_lora_node &
 
---
+++ sensor_check/src/main.cpp
@@ -0,0 +1,99 @@
+#include <ros/ros.h>
+#include <wiringPi.h>
+#include "life_msgs/Status.h"
+#define RED 17
+#define GREEN 27
+#define BLUE 22
+
+void IR(const life_msgs::Status& msg);
+void Cam(const life_msgs::Status& msg);
+void Imu(const life_msgs::Status& msg);
+void Motor(const life_msgs::Status& msg);
+
+
+bool IR_ok = false;
+bool Cam_ok = false;
+bool Imu_ok = false;
+bool Motor_ok = false;
+int main (int argc, char **argv)
+{
+    ros::init(argc, argv, "SENSOR_CHECK");
+    ros::NodeHandle nh;
+    ros::Subscriber sub1 = nh.subscribe("/Status/IR", 1 ,IR);
+    ros::Subscriber sub2 = nh.subscribe("/Status/Cam", 1 ,Cam);
+    ros::Subscriber sub3 = nh.subscribe("/Status/Imu", 1 ,Imu);
+    ros::Subscriber sub4 = nh.subscribe("/Status/Motor", 1 ,Motor);
+    
+    wiringPiSetupGpio();
+    pinMode(RED, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(BLUE, OUTPUT);
+    
+    digitalWrite(RED,LOW);
+    digitalWrite(GREEN,HIGH);
+    digitalWrite(BLUE,LOW);
+    ROS_INFO("STATUS SET UP OK");
+    
+    while (ros::ok())
+    {
+    	if(IR_ok & Cam_ok & Imu_ok & Motor_ok ){
+    		digitalWrite(RED,LOW);
+    		digitalWrite(GREEN,LOW);
+    		digitalWrite(BLUE,HIGH);
+    		ROS_INFO("ALL SENSOR IS GOOD");	
+    	}
+    	else{
+    		digitalWrite(RED,LOW);
+    		digitalWrite(GREEN,HIGH);
+    		digitalWrite(BLUE,LOW);
+    	}
+        ros::Duration(1).sleep();
+        ros::spinOnce();
+        
+    }
+    return 0;
+}
+
+
+void IR(const life_msgs::Status& msg){
+	IR_ok = msg.good;
+	if(!IR_ok){
+		ROS_INFO("IR Status ERROR");
+	}
+	else{
+		ROS_INFO("IR Status GOOD");
+	}
+}
+
+void Cam(const life_msgs::Status& msg){
+	Cam_ok = msg.good;
+	if(!Cam_ok){
+		ROS_INFO("Cam Status ERROR");
+	}
+	else{
+		ROS_INFO("Cam Status GOOD");
+	}
+}
+
+void Imu(const life_msgs::Status& msg){
+	Imu_ok = msg.good;
+	if(!Imu_ok){
+		ROS_INFO("Imu Status ERROR");
+	}
+	else{
+		ROS_INFO("Imu Status GOOD");
+	}
+}
+
+void Motor(const life_msgs::Status& msg){
+	Motor_ok = msg.good;
+	if(!Motor_ok){
+		ROS_INFO("Motor Status ERROR");
+	}
+	else{
+		ROS_INFO("Motor Status GOOD");
+	}
+}
+
+
+
